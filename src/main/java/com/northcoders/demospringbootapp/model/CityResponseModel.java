ee2bff986f2598ded23af09180066fd57bdaa99c
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-28 13:50:06 +0100

    fix: correct the field name to match api output

--- src/main/java/com/northcoders/demospringbootapp/model/CityResponseModel.java
+++ src/main/java/com/northcoders/demospringbootapp/model/CityResponseModel.java
@@ -1,2 +1,9 @@
-package com.northcoders.demospringbootapp.model;public class CityResponseModel {
-}
+package com.northcoders.demospringbootapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record CityResponseModel
+        (ArrayList<CityModel> results){}
