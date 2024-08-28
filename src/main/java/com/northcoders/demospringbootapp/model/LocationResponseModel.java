3d35d2f187531f3b3a0470c11706cacdc69558b3
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-28 15:32:04 +0100

    feat: create locationResponseModel for json

--- src/main/java/com/northcoders/demospringbootapp/model/LocationResponseModel.java
+++ src/main/java/com/northcoders/demospringbootapp/model/LocationResponseModel.java
@@ -1,2 +1,9 @@
-package com.northcoders.demospringbootapp.model;public class LocationResponseModel {
+package com.northcoders.demospringbootapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record LocationResponseModel(LocationModel results) {
 }
