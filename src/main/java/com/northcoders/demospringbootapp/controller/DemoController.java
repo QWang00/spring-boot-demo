85d9e0e5d93ecc64bb5d58b07e581c5cd0a85fdb
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-28 14:24:45 +0100

    feat: implement getFirstCityCoordinates in DemoController

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -6,6 +6,9 @@ import com.northcoders.demospringbootapp.model.CityResponseModel;
 import com.northcoders.demospringbootapp.model.PersonModel;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("api/v1")
 public class DemoController {
@@ -34,5 +37,14 @@ public class DemoController {
         CityResponseModel cities = GenericDAO.mapJsonToPojo(cityResponse, CityResponseModel.class);
 
          return cities;
+    }
+
+    @GetMapping("/coordinates")
+    public Map<String, Object> getFirstCityCoordinates(@RequestParam String city){
+        Map<String, Object> coordinatesMap = new HashMap<>();
+        CityResponseModel cities = getCities(city, 1);
+        coordinatesMap.put("Longitude", cities.results().get(0).longitude());
+        coordinatesMap.put("Latitude", cities.results().get(0).latitude());
+        return coordinatesMap;
     }
 }
--- src/main/java/com/northcoders/demospringbootapp/model/CoordinatesResponse.java
+++
@@ -1,2 +0,0 @@
-package com.northcoders.demospringbootapp.model;public class CoordinatesResponse {
-}
