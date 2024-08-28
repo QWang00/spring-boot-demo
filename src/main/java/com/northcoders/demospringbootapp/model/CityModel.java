65639e31677efb1cdd0ca09c98cb7634be3e6263
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-28 13:49:05 +0100

    fix: correct the field name to match api doc

--- src/main/java/com/northcoders/demospringbootapp/model/CityModel.java
+++ src/main/java/com/northcoders/demospringbootapp/model/CityModel.java
@@ -1,9 +1,27 @@
 package com.northcoders.demospringbootapp.model;
 
-public record City (int id, String name, Float latitude, Float longitude,
-                    Float elevation, String timeZone,
-                    String feature_code, String country_code,
-                    String country, int country_id, int population,
-                    String[] postcodes, String admin1, String admin2,
-                    String admin3, String admin4, int admin1_id,
-                    int admin2_id, int admin3_id, int admin4_id) {}
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record CityModel(int id,
+                        String name,
+                        Float latitude,
+                        Float longitude,
+                        Float elevation,
+                        String feature_code,
+                        String country_code,
+                        String country,
+                        int country_id,
+                        int population,
+                        ArrayList<String> postcodes,
+                        String admin1,
+                        String admin2,
+                        String admin3,
+                        String admin4,
+                        int admin1_id,
+                        int admin2_id,
+                        int admin3_id,
+                        int admin4_id,
+                        String timezone) {}
