45f58da47a7bc4ffc5c0633442a9cc80ada38b2e
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-28 15:32:39 +0100

    feat: implement getSunriseSunset in controller

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.northcoders.demospringbootapp.controller;
 
 import com.northcoders.demospringbootapp.dao.GenericDAO;
 import com.northcoders.demospringbootapp.model.CityResponseModel;
+import com.northcoders.demospringbootapp.model.LocationResponseModel;
 import com.northcoders.demospringbootapp.model.PersonModel;
 import org.springframework.web.bind.annotation.*;
 
@@ -47,4 +48,21 @@ public class DemoController {
         coordinatesMap.put("Latitude", cities.results().get(0).latitude());
         return coordinatesMap;
     }
+
+    @GetMapping("/sunrise-sunset")
+    public Map<String, Object> getLocation(@RequestParam float latitude, @RequestParam float longitude){
+        Map<String, Object> sunsetSunriseMap = new HashMap<>();
+        String[] params = new String[]{
+                "lat="+latitude,
+                "lng=" + longitude};
+        String locationResponse = GenericDAO.getJsonResponse("https://api.sunrisesunset.io/json", params, new String[]{});
+        LocationResponseModel location = GenericDAO.mapJsonToPojo(locationResponse, LocationResponseModel.class);
+        sunsetSunriseMap.put("Sunset", location.results().sunset());
+        sunsetSunriseMap.put("Sunrise", location.results().sunrise());
+
+        return sunsetSunriseMap;
+
+
+    }
+
 }
