21d9f336cde918958e0cdc127e2dc9fe63711379
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-28 15:31:16 +0100

    feat: create locationModel

--- src/main/java/com/northcoders/demospringbootapp/model/LocationModel.java
+++ src/main/java/com/northcoders/demospringbootapp/model/LocationModel.java
@@ -1,2 +1,22 @@
-package com.northcoders.demospringbootapp.model;public class LocationModel {
+package com.northcoders.demospringbootapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record LocationModel (String date,
+                             String sunrise,
+                             String sunset,
+                             String first_light,
+                             String last_light,
+                             String dawn,
+                             String dusk,
+                             String solar_noon,
+                             String golden_hour,
+                             String day_length,
+                             String timezone,
+                             int utc_offset){
+
 }
