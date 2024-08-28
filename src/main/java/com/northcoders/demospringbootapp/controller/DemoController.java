fbf09fb70781c1e9a819687ba7f3ece86019bcf8
Emily Rhodes <erhodes.345@gmail.com>
2024-08-28 11:05:10 +0100

    add Task 12

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.northcoders.demospringbootapp.controller;
 
 
+import com.northcoders.demospringbootapp.model.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +13,16 @@ public class DemoController {
     public String getGreetings(){
         return "Hello there!";
     }
+
+
+    Person person = new Person("Emma", 18, "emma18@gmail.com", "England", "Roast dinner");
+
+    @GetMapping("/helloEmma")
+    public Person greeterEmma (){
+        return person;
+    }
+
+
 }
+
+
