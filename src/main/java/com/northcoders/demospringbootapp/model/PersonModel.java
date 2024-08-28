1708225f3ee8fe16d40acfe8b06fa4dcc3df2068
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-28 13:54:48 +0100

    feat: create person model for getGreeting controller

--- src/main/java/com/northcoders/demospringbootapp/model/PersonModel.java
+++ src/main/java/com/northcoders/demospringbootapp/model/PersonModel.java
@@ -1,4 +1,4 @@
 package com.northcoders.demospringbootapp.model;
 
-public record Person(String name, int age, String email, String placeOfBirth, String favouriteFood) {
+public record PersonModel(String name, int age, String email, String placeOfBirth, String favouriteFood) {
 }
