2965cf820bc7c77a0b87ea1f0a9ad54f82a55dd1
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-28 13:53:49 +0100

    feat: create person model for getGreetings in controller

--- src/main/java/com/northcoders/demospringbootapp/model/PostcodeModel.java
+++ src/main/java/com/northcoders/demospringbootapp/model/PostcodeModel.java
@@ -1,2 +1,9 @@
-package com.northcoders.demospringbootapp.model;public record Postcode() {
+package com.northcoders.demospringbootapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record PostcodeModel(ArrayList<String> postcodes) {
 }
